cacc38e53bb15d3bffc4476a7f6a3a93163e932c
Joshua T Chukwu <JoshuaT.Chukwu@gmail.com>
2023-03-04 16:00:41 +0100

    test first use case

--- MovieApp.Contracts/Common/APIRoutes.cs
+++ MovieApp.Contracts/Common/APIRoutes.cs
@@ -15,7 +15,8 @@ namespace MovieApp.Contracts.Common
             public const string USER_LOGIN = Base + "/users/login";
             public const string USER_PROFILE_GET = Base + "/users/profile/get";
             public const string USER_VERIFY = Base + "/users/verify";
-           
+            public const string SEARCH_MOVIE = Base + "/users/search/movie";
+
         }
     }
 }
---
+++ MovieApp/Controllers/v1/UsersController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MovieApp.Contracts.Common;
+using MovieApp.Handlers;
+using MovieApp.Helpers;
+using MovieApp.Repository.Interface;
+using static MovieApp.Contracts.Common.AuxillaryObjs;
+using static MovieApp.Contracts.Common.OMDBAPIRequestObjs;
+using static MovieApp.Contracts.User_Identity_Obj.UsersOperationalObjs;
+
+namespace GOSBackend.Controllers.v1
+{
+    [AuthorizationMethod]
+    public class UsersController : ControllerBase
+    {
+        private readonly IHttpContextAccessor _contextAccessor;
+        private readonly ILogger<UsersController> _logger;
+        private readonly IUsersInterface _service;
+    
+        public UsersController(IHttpContextAccessor httpContextAccessor, ILogger<UsersController> logger, IUsersInterface usersInterface)
+        {
+          _contextAccessor = httpContextAccessor;
+            _logger = logger;
+            _service = usersInterface;
+            
+        }
+        [HttpGet(APIRoutes.Users.SEARCH_MOVIE)]
+        public ActionResult<SearchResponseObj> SearchMovie([FromQuery] SearchParams model)
+        {
+            try
+            {
+                var response =  _service.seachMovie(model);
+                if (response.Status.IsSuccess)
+                    return Ok(response);
+                return BadRequest(response);
+            }
+            catch (Exception ex)
+            {
+
+                var errorCode = ErrorID.Generate(5);
+                _logger.LogError($"ErrorID : {errorCode} Ex : {ex?.InnerException?.Message ?? ex?.Message} ErrorStack : {ex?.StackTrace}");
+                return BadRequest(new SearchResponseObj
+                {
+                    Status = new ApiResponse { IsSuccess = false,  FriendlyMessage = ex?.Message ??"", TechnicalMessage = ex?.InnerException?.Message ?? ex?.Message ?? "", ErrorCode = errorCode  }
+                });
+            }
+        }
+
+    }
+}
