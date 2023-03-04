11086848640b8c82415c26869a5cf7840c68df5f
Joshua T Chukwu <JoshuaT.Chukwu@gmail.com>
2023-03-04 14:21:19 +0100

    initial migration completed

--- MovieApp/SqlTables/MovieSearch.cs
+++ MovieApp/SqlTables/MovieSearch.cs
@@ -1,9 +1,14 @@
-﻿namespace MovieApp.SqlTables
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieApp.SqlTables
 {
     public class MovieSearch
     {
+        [Key]
+        public int MovieSearchId { get; set; }
         public string UserId { get; set; } = string.Empty;
         public string MovieName { get; set; } = string.Empty;
         public DateTime DateSearch { get; set; } = DateTime.Now;
+        public Users User { get; set; } = new Users();
     }
 }
